82d68f928e2f9d6272312f857f2399029f499624
neo-zerber <56646978+neo-zerber@users.noreply.github.com>
2022-12-06 15:31:29 +0200

    Add files via upload

---
+++ domain.py
@@ -0,0 +1,67 @@
+import whois
+import smtplib
+from datetime import datetime
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+
+def get_domain_expiration_date(domains):
+    domains_info = []
+
+    for domain in domains:        
+        expiration_date = whois.whois(domain).expiration_date
+        w = str(expiration_date)[0:10]
+        if isinstance(expiration_date, list):
+            days_diff = (expiration_date[0] - datetime.now()).days
+        elif isinstance(expiration_date, datetime):
+            days_diff = (expiration_date - datetime.now()).days
+        elif isinstance(expiration_date, str):
+            days_diff = (
+                datetime.strptime(expiration_date[:-3], "%Y-%m-%d %H:%M:%S")
+                - datetime.now()
+            ).days
+ 
+        else:
+            print(f"Domain {domain}, wrong expiration_date={expiration_date} type={type(expiration_date)}")
+            days_diff = -1
+        domains_info.append((domain, days_diff, w))
+        domains_info.sort(key=lambda x: x[1])
+    result = [(domain, f"{days} left", w) for domain, days, w in domains_info]
+
+    return result
+
+def send_email(message4):
+    sender = "Yevhen.Nikolskiy@mail.com.ua"
+    password = "*********"
+    msg = MIMEMultipart()
+    body = message1
+    msg['From'] = sender
+    msg.attach(MIMEText(body, 'plain'))
+    text = msg.as_string()
+    server = smtplib.SMTP("mail.com", 465)
+    server.starttls()
+
+    try: 
+        server.login(sender, password)
+        server.sendmail(sender, "Yevhen.Nikolskyi@com.ua", f"Subject: Expiration Domains in DAYS!\n{text}")
+
+        return "The massege send"
+    except Exception as _ex:
+        return f"{_ex}\nCheck your"
+
+def main():
+    print(send_email(message4=message1))
+    
+
+if __name__ == "__main__":
+    domains = ["dragon-capital.net", "dragon-capital.org", "dragon-capital.info", "TheDrgn.com", "ureh-pe.com", "ucpih.com", "nupeh.com", "dragon-capital.ua", "dccl.com.cy", "dragon-capital.cy", 
+    "dragon-capital.com.ua", "dragon-capital.kiev.ua", "dragon.ua", "drakon.ua", "dcf.com.ua", "dragon-pm.com.ua", "o-r.com.ua", "green-hills.com.ua",
+    "green-hills.ua", "new.green-hills.ua", "wgl.com.ua", "ces.org.ua", "premier-pm.com.ua", "prime-bc.com.ua", "eurasia-bc.com.ua",
+    "piramida.ua", "nupeh-cz.com", "kmzindustries.ua", "variant-ab.com.ua", "vgardens.com.ua", "skypark.ua", "eco-tower.zp.ua", "horizon-park.com.ua", "aladdin.ua",
+    "platinum-bc.com.ua", "victoriagardens.com.ua", "vg-offices.com", "viking-bc.com.ua", "grand-bc.com.ua", "obolon-plaza.com.ua", "pbpark.com.ua", "smartplaza-obolon.com.ua", "green-hills.fitness", "pampik.com.ua", "pampik.com", "pampik.kiev.ua", "e-distribution.com.ua", "parfums.ua", "parf.top", "m10.com.ua",
+    "m-10.com.ua", "e-40.com.ua", "e40.com.ua", "borychiv.com.ua", "pravda.ua", "tablo.com.ua", "kievpravda.com.ua", "adnet.com.ua", "tabloid.com.ua", "tabloid.net.ua", "narodnapravda.com.ua", "tabloid.org.ua", "pravda.fm", "pravda.com", "pravda.com.ua",
+    "epravda.com.ua", "greenleaf.com.ua", "rau.ua", "truskavetska.com.ua", "truskavetska.com", "ua-ua.org", "dilovyi-bc.com.ua", "obolon-residences.com.ua"]
+    domains_info = get_domain_expiration_date(domains)
+    message1 = ('\t\n'.join([str(item) for item in domains_info]))
+    print(message1)
+    main()
